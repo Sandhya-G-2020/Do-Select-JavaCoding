07e0ad4668ae0dd694868aff01bbd7ead20210f0
Sandhya-G-2020 <71721402+Sandhya-G-2020@users.noreply.github.com>
2022-04-21 14:48:37 +0530

    Add files via upload

---
+++ Day-2/Customer.java
@@ -0,0 +1,66 @@
+package definedexception;
+
+//import java.util.Collections;
+
+public class Customer {
+	String name;
+    long mobile;
+    char key;
+
+	
+	public Customer(String name , long mobile , char key) {
+		this.name=name;
+	    this.mobile=mobile;
+	    this.key=key;
+
+		
+	}
+	
+	public String encrypt() {
+		if(name.indexOf(key)>=0) {
+			return "Already Encrypted";
+		}
+		else {
+			long dig=mobile;
+			while(dig >= 10)
+			{
+				dig = dig / 10;
+			}
+			int n=(int)dig;
+			
+				//String result = name + String.join("", Collections.nCopies(n, "#"));
+			
+				//return result;
+			
+			StringBuilder s = new StringBuilder(name);
+		    for(int i=0; i<n; i++){
+		        s.append(key);
+		    }
+		    return s.toString();
+		}
+		
+	}
+
+	public long minCount() {
+		long min,rem;
+		long temp=mobile;
+		min=temp%10;
+	    while(temp>0) {
+	    	rem=temp%10;
+	    	if(min>rem)
+	    		min=rem;
+	    	temp=temp/10;
+	    }
+		return min;
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Customer customer = new Customer("Doselect", 29999992, '#');
+		System.out.println(customer.encrypt());
+		System.out.println(customer.minCount());
+
+
+	}
+
+}
+
